8291a82ca916e752ca80694546ac3785a6b14d2f
Eden Mesfun <emesfun@gmail.com>
2018-03-05 16:10:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
             binding.pry
-      student = self.new
+      student = self
     end
 
     #Scraper.scrape_profile_page(students_array)
