9d82a0578de4543790ed76d9e07fb60196e51d79
Eden Mesfun <emesfun@gmail.com>
2018-03-05 16:01:37 +0000

    automatically backed up by learn

--- lib/testing.rb
+++ lib/testing.rb
@@ -1,3 +1,10 @@
+1) Student .create_from_collection uses the Scraper class to create new students with the correct name and location.
+    Failure/Error: expect(Student.class_variable_get(:@@all).first.name).to eq("Alex Patriquin")
+    NoMethodError:
+      undefined method `name' for nil:NilClass
+    # ./spec/student_spec.rb:42:in `block (3 levels) in <top (required)>'
+---------
+
 def self.scrape_profile_page(profile_url)
     html = File.read(open(profile_url))
     profile_page = Nokogiri::HTML(html)
