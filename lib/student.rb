5676c7fbd8132e9a5b39a7dad239eb05d9c0ef98
Eden Mesfun <emesfun@gmail.com>
2018-03-05 15:19:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,8 +8,6 @@ class Student
   def initialize(student_hash)
     @name = student_hash[:name]
     @location = student_hash[:location]
-
-    @@all << name
   end
 
   def self.create_from_collection(students_array)
