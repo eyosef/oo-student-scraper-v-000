e804dc1518bcb81535e324e514ab2715a65663fa
Eden Mesfun <emesfun@gmail.com>
2018-03-05 15:06:58 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -46,12 +46,13 @@ class Scraper
         profile_page.css("div.social-icon-container a")[1].attribute("href").value if all_profiles[:linkedin] = linkedin
         profile_page.css("div.social-icon-container a")[2].attribute("href").value if all_profiles[:github] = github
         profile_page.css("div.social-icon-container a")[3].attribute("href").value if all_profiles[:blog] = blog
-        profile_page.css(".profile-quote").text if all_profiles[:profile_quote] = profile_quote
-        profile_page.css(".details-container p").children.text if all_profiles[:bio] = bio
+        # DELETE profile_page.css(".profile-quote").text if all_profiles[:profile_quote] = profile_quote
+        # DELETE profile_page.css(".details-container p").children.text if all_profiles[:bio] = bio
       end #each
         profile_quote = profile_page.css(".profile-quote").text
         bio = profile_page.css(".details-container p").children.text
-        
+        all_profiles[:profile_quote] = profile_quote
+        all_profiles[:bio] = bio
         all_profiles
 
     end #method
