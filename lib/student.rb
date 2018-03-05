fac42a7d1485cebfa1d64c11fe065b988ed75dc3
Eden Mesfun <emesfun@gmail.com>
2018-03-05 15:17:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,11 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
-    @location = student_hash[:location]
+    @name = name
+    @location = location
+
+    #@name = student_hash[:name]
+    #@location = student_hash[:location]
     @@all << @name
   end
 
