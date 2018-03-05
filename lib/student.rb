0ef324c34937a09d108c500b47339a9ffbd7e30a
Eden Mesfun <emesfun@gmail.com>
2018-03-05 17:08:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,7 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |student_attribute|
           binding.pry
-      @twitter = student_attribute[:twitter]
+      @twitter = student_attribute[1]
       # @linkedin = student_attribute[:linkedin]
       # @github = student_attribute[:github]
       # @blog = student_attribute[:blog]
