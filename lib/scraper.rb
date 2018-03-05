3879fd8889ed123ab8ad1da26828fca69838b83f
Eden Mesfun <emesfun@gmail.com>
2018-03-05 15:13:08 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -44,15 +44,15 @@ class Scraper
           all_profiles[:blog] = link
         end
 
-        link.include?(".com") if all_profiles[:github] = link
-
-
-        twitter = profile_page.css("div.social-icon-container a").attribute("href").value
-        linkedin = profile_page.css("div.social-icon-container a")[1].attribute("href").value
-  
-        github = profile_page.css("div.social-icon-container a")[2].attribute("href").value #profile_page.css("div.social-icon-container a")[2].attribute("href").value
-        blog = profile_page.css("div.social-icon-container a")[3].attribute("href").value
-
+        # link.include?(".com") if all_profiles[:github] = link
+        #
+        #
+        # twitter = profile_page.css("div.social-icon-container a").attribute("href").value
+        # linkedin = profile_page.css("div.social-icon-container a")[1].attribute("href").value
+        #
+        # github = profile_page.css("div.social-icon-container a")[2].attribute("href").value #profile_page.css("div.social-icon-container a")[2].attribute("href").value
+        # blog = profile_page.css("div.social-icon-container a")[3].attribute("href").value
+        #
 
         #all_profiles = {:twitter => twitter, :linkedin => linkedin, :github => github, :blog => blog, :profile_quote => profile_quote, :bio => bio}
 
