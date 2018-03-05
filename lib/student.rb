ed467e765d092fa12dc07c53eb4b548da0f14722
Eden Mesfun <emesfun@gmail.com>
2018-03-05 16:17:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
     students_array.each do |student|
       @name = student[:name]
       @location = student[:location]
-      @@all << self
+      @@all << s
       binding.pry
     end
 
