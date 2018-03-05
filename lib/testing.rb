d488d1479b3707e8002f45e0b5764c1c2214dc8b
Eden Mesfun <emesfun@gmail.com>
2018-03-05 16:33:31 +0000

    automatically backed up by learn

--- lib/testing.rb
+++ lib/testing.rb
@@ -3,3 +3,13 @@
     NoMethodError:
       undefined method `name' for nil:NilClass
     # ./spec/student_spec.rb:42:in `block (3 levels) in <top (required)>'
+
+    @@all = [#<Student:0x00000002ddc730 @location="New York, NY", @name="Alex Patriquin">]
+
+    students_array = [{:name=>"Alex Patriquin", :location=>"New York, NY"},
+ {:name=>"Bacon McRib", :location=>"Kansas City, MO"},
+ {:name=>"Alisha McWilliams", :location=>"San Francisco, CA"},
+ {:name=>"Daniel Fenjves", :location=>"Austin, TX"},
+ {:name=>"Arielle Sullivan", :location=>"Chicago, IL"},
+ {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"},
+ {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}]
