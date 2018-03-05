20ee1218b5f8f67e59ae0d1a79b38507d9d4ce3d
Eden Mesfun <emesfun@gmail.com>
2018-03-05 16:50:18 +0000

    automatically backed up by learn

--- lib/testing.rb
+++ lib/testing.rb
@@ -6,11 +6,15 @@
 
     @@all = [#<Student:0x00000002ddc730 @location="New York, NY", @name="Alex Patriquin">]
 
-    students_array =
-           [{:name=>"Alex Patriquin", :location=>"New York, NY"},
-           {:name=>"Bacon McRib", :location=>"Kansas City, MO"},
-           {:name=>"Alisha McWilliams", :location=>"San Francisco, CA"},
-           {:name=>"Daniel Fenjves", :location=>"Austin, TX"},
-           {:name=>"Arielle Sullivan", :location=>"Chicago, IL"},
-           {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"},
-           {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}]
+  
+    attributes_hash = 
+
+    {:twitter=>"someone@twitter.com",
+     :linkedin=>"someone@linkedin.com",
+     :github=>"someone@github.com",
+     :blog=>"someone@blog.com",
+     :profile_quote=>
+      "\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+     :bio=>
+      "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 peopl
+    e and 3000+ cows), and Tokyo for elementary/middle school."}
